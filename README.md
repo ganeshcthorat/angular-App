349d84732a227b2956b4a06e55bc69253d0a4a4a
ganeshcthorat <ganeshcthorat@gmail.com>
2019-05-08 11:30:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-yyecvz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yyecvz)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+movie=[  {no:1, film:'The Transporter', Geners: 'Action', Release: 'August 23'},
+  {no:2, film:'Homes', Geners: 'Action', Release: 'August 24'},
+  {no:3, film:'Minions', Geners: 'Sci-fi', Release: 'August 24'},
+  {no:4, film:'Spider Man', Geners: 'old', Release: 'August 25'},
+  {no:5, film:'Thor', Geners: 'Horror', Release: 'August 26'},
+  {no:6, film:'Avenger', Geners: 'Action', Release: 'August 25'},
+
+ 
+];
+
+}
