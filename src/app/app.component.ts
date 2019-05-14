c3274721d9af5dd31a883613370c422e4c4bc4c0
ganeshcthorat <ganeshcthorat@gmail.com>
2019-05-14 18:34:28 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+ 
 })
+
 export class AppComponent {
+  show: boolean = true;
+  isLoggedIn = true;
+  loggedOut = false;
   name = 'Angular';
   imgUrl ='https://www.gstatic.com/webp/gallery/1.webp';
   isDisabled = false;
